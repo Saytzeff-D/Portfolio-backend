5412a4c5f6e1f58d2d7878093d6ff8a0989750a8
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-09 04:31:15 +0100

    configured the server

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
-const app = express()
-const port = 8000
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}))
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const mongoose = require('mongoose');
+const PortfolioRouter = require('./routes/portfolio.route');
+
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(cors({origin: '*'}))
+app.use('/portfolio', PortfolioRouter)
+const port = process.env.PORT
+
+app.get('/', (req, res)=> res.send('Welcome to my  Portfolio Server'))
+
+mongoose.connect(process.env.URI, (err)=>{
+    if(!err){
+        console.log('Mongo Database Connected')
+    }else{
+        console.log('Mongoose Connection Error')
+    }
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`My Portfolio server is listening on port ${port}!`))
