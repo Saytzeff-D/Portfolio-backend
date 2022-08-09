2cac925df2034eb9dbb1a800f53506b9bc233251
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-09 22:59:06 +0100

    expertise model

---
+++ models/expertise.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const expertiseSchema = mongoose.Schema({
+    expert: {type: String, required: true },
+    duration: { type: String, required: true }
+})
+
+const ExpertiseModel = mongoose.model('expertise_tbs', expertiseSchema)
+
+module.exports = ExpertiseModel
