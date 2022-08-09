bc9b969994253405d18af748730b0ad42c6e4399
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-09 22:59:43 +0100

    project model

--- models/projects.model.js
+++ models/projects.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const projectSchema = mongoose.Schema({
-    projectName: String,
-    projectDesc: String,
-    mode: String,
-    repoLink:  String,
-    deployedLink: String
+    projectName: {type: String, required: true},
+    projectDesc: {type: String, required: true},
+    mode: {type: String, required: true},
+    repoLink:  {type: String, required: true},
+    deployedLink: {type: String, required: true}
 })
 
 const ProjectModel = mongoose.model('project_tbs', projectSchema)
