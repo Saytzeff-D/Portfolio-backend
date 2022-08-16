d56932e97e9e88f7f30f93656792ecc2f9630dc3
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-16 12:11:28 +0100

    updated project model

--- models/projects.model.js
+++ models/projects.model.js
@@ -4,6 +4,7 @@ const projectSchema = mongoose.Schema({
     projectName: {type: String, required: true},
     projectDesc: {type: String, required: true},
     mode: {type: String, required: true},
+    majorLang: {type: String, required: true},
     repoLink:  {type: String, required: true},
     deployedLink: {type: String, required: true}
 })
