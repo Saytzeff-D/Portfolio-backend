0b194034a4b2bd6b95e762752615297e7b570454
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-19 23:37:48 +0100

    updated this model

--- models/education.model.js
+++ models/education.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const educationSchema = mongoose.Schema({
     cert: {type: String, required: true },
-    dateObtained: { type: String, required: true }
+    dateObtained: { type: String, required: true },
+    schoolObtained: { type: String, reuired: true }
 })
 
 const EducationModel = mongoose.model('education_tbs', educationSchema)
